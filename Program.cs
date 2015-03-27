f8e1fb6a2791dda80fca61b0706e7a01ed0eede8
keonnaamoni <keonnaevans@gmail.com>
2015-03-26 21:26:17 -0400

    Commit Changes

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net
+{
+    public class 
+    {
+        public netStudents()
+        {
+            
+        }
+
+
+    enum studentRanking
+    {
+        Grasshopper,
+        Journeyman,
+        Rockstar,
+        Ninja,
+        Jedi
+
+    }
+
+
+        static void Main(string[] args)
+        {
+
+            Keonna keonnaE = new KeonnaE();
+            keonnaE.Program = decimal 1;
+            keonnaE.HelpStudent();
+            keonnaE.Level();
+
+        }
+    }
+
+    class Keonna
+	{
+        public string Program { get; set; }
+        public string HelpStudent { get; set; }
+        public string Level { get; set; }
+	}   
+}
